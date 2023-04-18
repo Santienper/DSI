88f5ce2a83b66cda17fca7c915447d33999e4f98
luisja30 <luisjnav@ucm.es>
2023-04-18 12:52:35 +0200

    He hecho clases para los modos del list view del mainPage

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -22,9 +24,27 @@ namespace Trabajo_DSI
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public ObservableCollection<Mode> ListaModos { get; } = new ObservableCollection<Mode>();
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Cosntruye las listas de ModelView a partir de la lista Modelo 
+            if (ListaModos != null)
+                foreach (Mode modo in ModesInformation.GetAllModes())
+                {
+                    ViewMode VMode = new ViewMode(modo);
+                    ListaModos.Add(VMode);
+                }
+            base.OnNavigatedTo(e);
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Mode Sel = e.ClickedItem as Mode;
+            Description.Text = Sel.Explicacion;
         }
     }
 }
---
+++ ModesInformation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Trabajo_DSI
+{
+    public class Mode
+    {
+        public int Id { get; set; }
+        public string Explicacion { get; set; }
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+
+
+        public Mode() { }
+    }
+    public class ModesInformation
+    {
+        public static List<Mode> Modes = new List<Mode>()
+        {
+            new Mode()
+            {
+                Id=0,
+                Nombre="UN JUGADOR (vs CPU)",
+                Explicacion=" Modo de un jugador Enfrentante en solitario al modo Campaña.",
+                Imagen="Assets\\Images\\nueva partida iron man.jpg",
+            },
+            new Mode()
+            {
+                Id=1,
+                Nombre="MULTIJUGADOR",
+                Explicacion=" Modo multijugador Enfrentante a jugadores de todo el mundo o con amigos.",
+                Imagen="Assets\\Images\\vengadores multijugador.jpg",
+            }
+        };
+
+        public static IList<Mode> GetAllModes()
+        {
+            return Modes;
+        }
+    }
+    
+}
